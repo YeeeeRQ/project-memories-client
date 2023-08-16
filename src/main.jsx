a25b3557bb56abf5d9bbf23e0c863b5db591118a
YeeeeRQ <YeeeeRQ@outlook.com>
2023-08-16 09:20:33 +0800

    posts

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import './index.css'
 
 import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
