cdc59214f51683f8764ff31f2157c0a8cb137873
YeeeeRQ <YeeeeRQ@outlook.com>
2023-08-16 08:33:06 +0800

    Form & Create Post

--- src/components/Posts/Post/Post.jsx
+++ src/components/Posts/Post/Post.jsx
@@ -1,3 +1,4 @@
+import {Card, CardActions, CardContent,CardMedia, Button, Typography} from '@material-ui/core'
 import useStyles from './styles';
 const Post = () => {
   const classes = useStyles();
